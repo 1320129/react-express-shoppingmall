a9c741140536f77cd6146b3a43aaf20ae9522fb2
jaehyuck <shwogur3000@gmail.com>
2022-06-08 07:40:02 +0900

    업로드 페이지 제작중

--- client/src/components/utils/UpLoadFile.jsx
+++ client/src/components/utils/UpLoadFile.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropzone from "react-dropzone";
 import { Icon } from "antd";
+import Axios from "axios";
 
 function UpLoadFile() {
+  const [Images2, setImages2] = useState([]);
+  const dropHandler = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/fomr-data" },
+    };
+    formData.append("file", files[0]);
+
+    Axios.post("/api/product/image", formData, config).then((response) => {
+      if (response.data.success) {
+        setImages2([...Images2, response.data.filePath]);
+      } else {
+        alert("저장실패");
+      }
+    });
+  };
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+      <Dropzone onDrop={dropHandler}>
         {({ getRootProps, getInputProps }) => (
           <div
             style={{
@@ -23,6 +40,23 @@ function UpLoadFile() {
           </div>
         )}
       </Dropzone>
+      <div
+        style={{
+          display: "flex",
+          width: "350px",
+          height: "240px",
+          overflowX: "scroll",
+        }}
+      >
+        {Images2.map((image, index) => (
+          <div key={index}>
+            <img
+              style={{ minWidth: "300px", width: "300px", height: "240px" }}
+              src={`http://localhost:3000/${image}`}
+            ></img>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
