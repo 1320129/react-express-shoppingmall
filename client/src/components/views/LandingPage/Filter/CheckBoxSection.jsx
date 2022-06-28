2ad7d7fb0472b7c270e7b21ec42ee9063b3c9b4d
jaehyuck <shwogur3000@gmail.com>
2022-06-28 23:12:50 +0900

    체크된 값 찾고 빼주기

--- client/src/components/views/LandingPage/Filter/CheckBoxSection.jsx
+++ client/src/components/views/LandingPage/Filter/CheckBoxSection.jsx
@@ -2,11 +2,34 @@ import React from "react";
 import { Collapse } from "antd";
 import { Checkbox } from "antd";
 import { useEffect } from "react";
+import { useState } from "react";
 const { Panel } = Collapse;
 
 function CheckBoxSection(props) {
+  const [Check, setCheck] = useState([]);
+  const toggleHandler = (index) => {
+    let current = Check.indexOf(index);
+    const newChecked = [...Check];
+    if (current === -1) {
+      newChecked.push(index);
+    } else {
+      console.log(current);
+      newChecked.splice(current, 1);
+    }
+    setCheck(newChecked);
+    // console.log(Check);
+    console.log(newChecked);
+  };
   const CheckboxRender = props.list.map((item) => {
-    return <Checkbox key={item.id}>{item.name}</Checkbox>;
+    return (
+      <Checkbox
+        key={item.id}
+        onChange={() => toggleHandler(item.id)}
+        checked={false}
+      >
+        {item.name}
+      </Checkbox>
+    );
   });
   return (
     <Collapse defaultActiveKey={["1"]}>
